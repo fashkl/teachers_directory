02d74ac149d34fa74476f4f5ffa2a958022fe408
Mohamed Ali <mohamed.su37@gmail.com>
2021-08-28 02:47:41 +0400

    Update settings.py

--- teacher_directory/settings.py
+++ teacher_directory/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8l0m@o%_yc-&df2c)&5jy!9)#85gi-gs!18^$02fhgjwg$na5g'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
